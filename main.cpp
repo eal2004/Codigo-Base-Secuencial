c8affacb05676104ef619f054a1e901399d92123
eal2004 <carlos.alba5702@alumnos.udg.mx>
2026-05-27 20:47:41 -0600

    optimizacion: balanceo de carga dinamico y solucion de falso compartimiento

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Pixel {
 };
 
 void generateMandelbrot(std::vector<Pixel>& image) {
-    #pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(dynamic, 16)
     for (int y = 0; y < HEIGHT; ++y) {
         for (int x = 0; x < WIDTH; ++x) {
             double pr = 1.5 * (x - WIDTH / 2.0) / (0.5 * WIDTH) - 0.5;
@@ -69,6 +69,33 @@ void applyGaussianBlur(const std::vector<Pixel>& input, std::vector<Pixel>& outp
     }
 }
 
+void calculateHistogramAtomic(const std::vector<Pixel>& image, std::vector<int>& histR, std::vector<int>& histG, std::vector<int>& histB) {
+    #pragma omp parallel for
+    for (size_t i = 0; i < image.size(); ++i) {
+        #pragma omp atomic
+        histR[image[i].r]++;
+
+        #pragma omp atomic
+        histG[image[i].g]++;
+
+        #pragma omp atomic
+        histB[image[i].b]++;
+    }
+}
+
+void calculateHistogramReduction(const std::vector<Pixel>& image, std::vector<int>& histR, std::vector<int>& histG, std::vector<int>& histB) {
+    int* ptrR = histR.data();
+    int* ptrG = histG.data();
+    int* ptrB = histB.data();
+
+    #pragma omp parallel for reduction(+:ptrR[:256], ptrG[:256], ptrB[:256])
+    for (size_t i = 0; i < image.size(); ++i) {
+        ptrR[image[i].r]++;
+        ptrG[image[i].g]++;
+        ptrB[image[i].b]++;
+    }
+}
+
 void savePPM(const std::string& filename, const std::vector<Pixel>& image) {
     std::ofstream file(filename, std::ios::binary);
     file << "P6\n" << WIDTH << " " << HEIGHT << "\n255\n";
@@ -83,16 +110,31 @@ int main() {
     std::vector<Pixel> blurredImage(WIDTH * HEIGHT);
 
     double startMandelbrot = omp_get_wtime();
-    std::cout << "Iniciando Tarea A: Generando Mandelbrot 8K con OpenMP..." << std::endl;
+    std::cout << "Iniciando Tarea A: Generando Mandelbrot 8K..." << std::endl;
     generateMandelbrot(image);
     double endMandelbrot = omp_get_wtime();
     std::cout << "Tiempo Tarea A: " << endMandelbrot - startMandelbrot << " segundos." << std::endl;
 
     double startBlur = omp_get_wtime();
-    std::cout << "Iniciando Tarea B: Aplicando convolucion 2D pesada con OpenMP..." << std::endl;
+    std::cout << "Iniciando Tarea B: Aplicando convolucion 2D pesada..." << std::endl;
     applyGaussianBlur(image, blurredImage);
     double endBlur = omp_get_wtime();
     std::cout << "Tiempo Tarea B: " << endBlur - startBlur << " segundos." << std::endl;
+
+    std::vector<int> histR_atomic(256, 0), histG_atomic(256, 0), histB_atomic(256, 0);
+    std::vector<int> histR_reduct(256, 0), histG_reduct(256, 0), histB_reduct(256, 0);
+
+    double startAtomic = omp_get_wtime();
+    std::cout << "Iniciando Histograma Atomico..." << std::endl;
+    calculateHistogramAtomic(blurredImage, histR_atomic, histG_atomic, histB_atomic);
+    double endAtomic = omp_get_wtime();
+    std::cout << "Tiempo Histograma Atomico: " << endAtomic - startAtomic << " segundos." << std::endl;
+
+    double startReduct = omp_get_wtime();
+    std::cout << "Iniciando Histograma Reduccion..." << std::endl;
+    calculateHistogramReduction(blurredImage, histR_reduct, histG_reduct, histB_reduct);
+    double endReduct = omp_get_wtime();
+    std::cout << "Tiempo Histograma Reduccion: " << endReduct - startReduct << " segundos." << std::endl;
 
     std::cout << "Guardando resultado final..." << std::endl;
     savePPM("fractal_procesado_paralelo.ppm", blurredImage);
